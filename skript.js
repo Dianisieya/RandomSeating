6375446d30c3f346d6b45d5ccd10236fdcdb4d8f
Dianisieya <diana.chernaya102@gmail.com>
2025-06-10 13:03:14 +0300

    Added base files
    added main page, js skript and css file

---
+++ skript.js
@@ -0,0 +1,18 @@
+const guests = ['Диана', 'Ксюша', 'Лера'];
+
+function randomSeat() {
+
+    const shuffledGuests = [...guests].sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < 3; i++) {
+        const seatElement = document.getElementById(`seat${i + 1}`);
+        seatElement.textContent = shuffledGuests[i] || '-';
+        seatElement.style.backgroundColor = getRandomLightColor();
+    }
+}
+
+function getRandomLightColor() {
+    const colr = Math.floor(Math.random() * 360);
+    return `hsl(${colr}, 80%, 90%)`;
+}
+ 
