3aa95ba52ae32b824cd2548a8b894b53c0f28028
Dianisieya <diana.chernaya102@gmail.com>
2025-06-10 13:21:43 +0300

    changing function randomSeat
    changing the logic of array processing

--- skript.js
+++ skript.js
@@ -1,12 +1,11 @@
 const guests = ['Диана', 'Ксюша', 'Лера'];
 
 function randomSeat() {
-
-    const shuffledGuests = [...guests].sort(() => Math.random() - 0.5);
-
+    guests.sort(() => Math.random() - 0.5);
+    
     for (let i = 0; i < 3; i++) {
         const seatElement = document.getElementById(`seat${i + 1}`);
-        seatElement.textContent = shuffledGuests[i] || '-';
+        seatElement.textContent = guests[i];
         seatElement.style.backgroundColor = getRandomLightColor();
     }
 }
